aff14dfb65829903d2e0998198559c42c12d0a84
Yogesh Suhas Naikwadi <yogeshnaikwadi2000@gmail.com>
2023-12-10 22:37:48 +0530

    Ecommerce

--- backend/utils/jwtTokens.js
+++ backend/utils/jwtTokens.js
@@ -3,7 +3,7 @@ const setTokens = (user,status,res) =>{
     const token=user.getJwtToken();
 
     const option={
-        
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE *24 *60*60*1000),
         httpOnly:true,
         path: "/",
         secure: process.env.NODE_ENV === 'production',
